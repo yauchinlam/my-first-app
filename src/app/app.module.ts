3ebbdacae5c80feef051a15a34cb7eba57da47fc
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-03-09 20:32:52 -0500

    Added component automatically using Agile CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
